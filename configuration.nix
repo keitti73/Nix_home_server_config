15d84f3704204258411cb7903b7171f71941c836
keitti73 <112259942+keitti73@users.noreply.github.com>
2024-12-02 10:31:43 +0900

    disable wireplumber

--- configuration.nix
+++ configuration.nix
@@ -125,7 +125,7 @@ in
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-    wireplumber.enable = true;
+    #wireplumber.enable = true;
   };
 
   # Allow unfree packages
