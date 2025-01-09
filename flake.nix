bb94e0f3a5c12489949c54c4bc6fb7dc53c35517
keitti73 <keitti73@gmail.com>
2025-01-09 22:04:30 +0900

    home-managerの設定を追加し、ユーザー管理を有効化

--- flake.nix
+++ flake.nix
@@ -10,14 +10,29 @@
       # Optional but recommended to limit the size of your system closure.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, lanzaboote,...}: {
+  outputs = { self, nixpkgs, lanzaboote,home-manager,...}: {
     nixosConfigurations = {
       nixos-1 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.keitti73 = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
 
           lanzaboote.nixosModules.lanzaboote
 
