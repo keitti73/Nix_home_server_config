8aac96b0bb92a301961ecab05ebd013ea2e82f1a
keitti73 <keitti73@gmail.com>
2025-01-10 00:02:22 +0900

    ssh.nixのauthorized_keysファイルのユーザー名を修正

--- ssh.nix
+++ ssh.nix
@@ -8,7 +8,7 @@
   };
 
   users.users.keitti73.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjNVMfNot/wCC+9qSyIFYprV5UcWssXrji9ejD9VFgF keitti73@laptop" # content of authorized_keys file
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjNVMfNot/wCC+9qSyIFYprV5UcWssXrji9ejD9VFgF keitti73" # content of authorized_keys file
     # note: ssh-copy-id will add user@your-machine after the public key
     # but we can remove the "@your-machine" part
   ];
